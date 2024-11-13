63c39047853e6dec052d40c9f4a321e3ae264628
dptorrez <paolatrz1998@gmail.com>
2024-11-13 17:51:31 -0600

    Added API in JS for my search engine

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
+function getWeather(response) {
+    let temperatureElement = document.querySelector("#temperature");
+    let temperature = response.data.temperature.current;
+    let cityElement = document.querySelector("#city");
+
+    cityElement.innerHTML = response.data.city;
+    temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function searchingCity(city) {
+    //let apiKey = "bd9741tbdfabbd3bc4c44f3o556466c0";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=bd9741tbdfabbd3bc4c44f3o556466c0&units=metric`;
+    //console.log(apiUrl);
+    axios.get(apiUrl).then(getWeather);
+}
+
 function searchForInfo(event) {
     event.preventDefault();
     let cityEntered = document.querySelector("#city-entered");
-    let cityElement = document.querySelector("#city");
-    cityElement.innerHTML = cityEntered.value;
+    searchingCity(cityEntered.value);
 }
 
 
 let searchCityElement = document.querySelector("#city-search");
-searchCityElement.addEventListener("submit", searchForInfo)
+searchCityElement.addEventListener("submit", searchForInfo);
+
+searchingCity("Houston");
