6f88ec19694ab7e5028783af0dd22d3407eef7ae
dptorrez <paolatrz1998@gmail.com>
2024-11-13 21:07:37 -0600

    added an icon that changes based on the city

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ function getWeather(response) {
     let windElement = document.querySelector("#wind");
     let currentTimeElement = document.querySelector("#time");
     let date = new Date(response.data.time * 1000);
-
+    let iconElement = document.querySelector("#icon");
+    
+    iconElement.innerHTML = `<img src="${response.data.condition.icon_url}"class="weather-icon"/>`;
     currentTimeElement.innerHTML = showDate(date);
     windElement.innerHTML = `${response.data.wind.speed}km/h`;
     humidyElement.innerHTML = `${response.data.temperature.humidity}%`;
