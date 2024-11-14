c264df93c38af14990540e915d8a547159162463
dptorrez <paolatrz1998@gmail.com>
2024-11-13 19:00:15 -0600

    Injected current weather data

--- index.js
+++ index.js
@@ -2,9 +2,31 @@ function getWeather(response) {
     let temperatureElement = document.querySelector("#temperature");
     let temperature = response.data.temperature.current;
     let cityElement = document.querySelector("#city");
+    let descriptionElement = document.querySelector(".weather-description");
+    let humidyElement = document.querySelector("#humidity");
+    let windElement = document.querySelector("#wind");
+    let currentTimeElement = document.querySelector("#time");
+    let date = new Date(response.data.time * 1000);
 
+    currentTimeElement.innerHTML = showDate(date);
+    windElement.innerHTML = `${response.data.wind.speed}km/h`;
+    humidyElement.innerHTML = `${response.data.temperature.humidity}%`;
+    descriptionElement.innerHTML = response.data.condition.description;
     cityElement.innerHTML = response.data.city;
     temperatureElement.innerHTML = Math.round(temperature);
+    console.log(response);
+}
+function showDate(date) {
+    let minutes = date.getMinutes();
+    let hours = date.getHours();
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    let day = days[date.getDay()];
+
+    if (minutes < 10 ) {
+        minutes = `0${minutes}`;
+    }
+
+    return `${day} ${hours}:${minutes}`
 }
 
 function searchingCity(city) {
